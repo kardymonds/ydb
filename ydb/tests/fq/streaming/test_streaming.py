cba22aed092b8f4ea35d1815fb34638b2d0515af
Dmitry Kardymon <kardymon-d@ydb.tech>
2025-09-29 09:57:46 +0300

    YQ-4699 Check shared_ptr in metadata_loader (#25580)

--- ydb/tests/fq/streaming/test_streaming.py
+++ ydb/tests/fq/streaming/test_streaming.py
@@ -64,31 +64,47 @@ class TestStreamingInYdb(StreamingImportTestBase, TestYdsBase):
         assert result_sets1[0].rows[0]['time'] == b'lunch time'
         assert result_sets2[0].rows[0]['time'] == b'lunch time'
 
-    @pytest.mark.skip(reason="How to cancel queries?")
-    def test_read_topic_shared_reading_insert_to_topic(self):
-        sourceName = "source3"
-        self.init_topics(sourceName, partitions_count=10)
-        self.ydb_client.query(f"""
-            CREATE EXTERNAL DATA SOURCE `{sourceName}` WITH (
-                SOURCE_TYPE="Ydb",
-                LOCATION="{os.getenv("YDB_ENDPOINT")}",
-                DATABASE_NAME="{os.getenv("YDB_DATABASE")}",
-                SHARED_READING="TRUE",
-                AUTH_METHOD="NONE");""")
+#     @pytest.mark.skip(reason="How to cancel queries?")
+#     def test_read_topic_shared_reading_insert_to_topic(self):
+#         sourceName = "source3"
+#         self.init_topics(sourceName, partitions_count=10)
+#         self.ydb_client.query(f"""
+#             CREATE EXTERNAL DATA SOURCE `{sourceName}` WITH (
+#                 SOURCE_TYPE="Ydb",
+#                 LOCATION="{os.getenv("YDB_ENDPOINT")}",
+#                 DATABASE_NAME="{os.getenv("YDB_DATABASE")}",
+#                 SHARED_READING="TRUE",
+#                 AUTH_METHOD="NONE");""")
 
-        sql = f"""
-            $in = SELECT time FROM {sourceName}.`{self.input_topic}`
-                WITH (
-                    FORMAT="json_each_row",
-                    SCHEMA=(time String NOT NULL))
-                WHERE time like "%lunch%";
+#         sql = R'''
+#             CREATE STREAMING QUERY `{query_name}` AS
+#             DO BEGIN
+#                 $in = SELECT time FROM {source_name}.`{input_topic}`
+#                 WITH (
+#                     FORMAT="json_each_row",
+#                     SCHEMA=(time String NOT NULL))
+#                 WHERE time like "%lunch%";
+#                 INSERT INTO {source_name}.`{output_topic}` SELECT time FROM $in;
+#             END DO;'''
 
-            INSERT INTO {sourceName}.`{self.output_topic}` SELECT time FROM $in;"""
+# <<<<<<< HEAD
+#             INSERT INTO {sourceName}.`{self.output_topic}` SELECT time FROM $in;"""
 
-        self.ydb_client.query_async(sql)
-        self.ydb_client.query_async(sql)
-        time.sleep(3)
-        data = ['{"time": "lunch time"}']
-        expected_data = ['lunch time', 'lunch time']
-        self.write_stream(data)
-        assert self.read_stream(len(expected_data), topic_path=self.output_topic) == expected_data
+#         self.ydb_client.query_async(sql)
+#         self.ydb_client.query_async(sql)
+# =======
+#         self.ydb_client.query(sql.format(query_name="query1", source_name=sourceName, input_topic=self.input_topic, output_topic=self.output_topic))
+#         self.ydb_client.query(sql.format(query_name="query2", source_name=sourceName, input_topic=self.input_topic, output_topic=self.output_topic))
+# >>>>>>> d2f18549aec (YQ-4699 Check shared_ptr in metadata_loader (#25580))
+#         time.sleep(3)
+#         data = ['{"time": "lunch time"}']
+#         expected_data = ['lunch time', 'lunch time']
+#         self.write_stream(data)
+#         assert self.read_stream(len(expected_data), topic_path=self.output_topic) == expected_data
+# <<<<<<< HEAD
+# =======
+
+#         sql = R'''DROP STREAMING QUERY `{query_name}`;'''
+#         self.ydb_client.query(sql.format(query_name="query1"))
+#         self.ydb_client.query(sql.format(query_name="query2"))
+# >>>>>>> d2f18549aec (YQ-4699 Check shared_ptr in metadata_loader (#25580))
